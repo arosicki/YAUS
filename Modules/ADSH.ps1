512aa07c870ad5588d4919adc9c30f52117ede2e
arosicki <justnobody856@gmail.com>
2021-02-10 17:05:01 +0100

    Implement domain controller recognision

--- Modules/ADSH.ps1
+++ Modules/ADSH.ps1
@@ -11,6 +11,11 @@
 # following the pattern [*c.xml]
 #
 ################################################
+function checkOS {
+    Clear-Host
+    Write-Output "Checking prerequisities"
+    return (Get-ComputerInfo).OsProductType
+}
 function getFiles {
     $count = 0
     [xml]$Script:xmlDB = '<?xml version="1.0" encoding="utf-8"?><Commands></Commands>'
@@ -19,7 +24,7 @@ function getFiles {
     $ADSHfiles | ForEach-Object {
         [xml]$xmlFile = Get-Content -Path $_.FullName
         $count++
-        # Clear-Host
+        Clear-Host
         Write-Host -NoNewline "Getting commands from XML files $nl $count of $nrOfFiles files $nl"
         ForEach ($XmlNode in $xmlFile.DocumentElement.ChildNodes) {
             $Script:xmlDB.DocumentElement.AppendChild($Script:xmlDB.ImportNode($XmlNode, $true)) > $null
@@ -27,7 +32,7 @@ function getFiles {
     }
 }
 function implementCommands {
-    # Clear-Host
+    Clear-Host
     Write-Host -NoNewline "Implementing" ($Script:xmlDB.Commands.Command.Name).Count "commands $nl"
     $date = Get-Date -Format "yyyymmddHHmm"
     $file = "tmp" + $date + ".psm1"
@@ -60,13 +65,28 @@ function implementCommands {
             $count++
         }
         $manual = $nl + "<# " + $nl + ".SYNOPSIS " + $nl + $command.synopsis + $nl + ".DESCRIPTION " + $nl + $command.description + $argumentsManual + $examplesMaual + $nl +"#>" + $nl
-        "function " + $command.Name + "{" + $manual + "param(" + $arguments + $nl + ")" + $nl + $command.code + $nl + "}" >> $file
+        "function Y-" + $command.Name + "{" + $manual + "param(" + $arguments + $nl + ")" + $nl + $command.code + $nl + "}" >> $file
     }
     Import-Module $file
     Remove-Item $file
 }
 function ADSH {
+    $osType = checkOS
+    if($osType -eq 1) {
+        Clear-Host
+        Read-Host "You do not run windows server..."$nl"In order to use ADSH run script in wndows server..."$nl"Exiting to YAUS.."
+        return
+    }
+    elseif ($osType -eq 3) {
+        Clear-Host
+        $ifUpgrade = Read-Host "Your server is not domain controller do you want to promote it to domain controller(y/n)[Default:y]"$nl
+        if ($ifUpgrade -eq "n") {
+            return
+        }
+    }
+    runCLI
+    $Script:version = "v1.0.0"
     getFiles
     implementCommands
-    Test "Nigga"
+    Y-Test "Test"
 }
